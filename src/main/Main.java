a1eda665ef3448b37a5b31aaab8086947c372a34
Sterenn <slh35@hotmail.fr>
2023-05-16 19:55:06 +0200

    Version qui "marche" avec collision, background et items

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,13 +12,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//Fenêtre de lancement du jeu
+		//Fenetre de lancement du jeu
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setTitle("ESIR1 - Projet Prog");
 		
-		//Ajout du panel du jeu et démarrage du thread principal
+		//Ajout du panel du jeu et demarrage du thread principal
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
 		window.pack();
